c05d8456e795baaab052ca5d08f0020aeb9dec36
MaLkoLmX <kadirovfamily@rambler.ru>
2017-09-16 16:15:20 +0400

    Проверка итогового проекта

--- js/scripts.js
+++ js/scripts.js
@@ -12,26 +12,26 @@ link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
 	login.focus();
-  });
+});
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
-  });
+});
 
 form.addEventListener("submit", function (evt) {
 	if (!name.value || !e-mail.value || !text.value) {
-      evt.preventDefault();
-      console.log("Заполните поля");
-    } else {
-      localStorage.setItem("login", name.value);
-    }
-  });
+  evt.preventDefault();
+  popup.classList.add("modal-error");
+  } else {
+  localStorage.setItem("login", name.value);
+  }
+});
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-      }
-    }
-  });
+  if (evt.keyCode === 27) {
+  if (popup.classList.contains("modal-show")) {
+  popup.classList.remove("modal-show");
+  }
+  }
+});
