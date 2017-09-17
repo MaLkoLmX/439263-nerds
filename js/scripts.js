03cf45bd900ac07ae17c29492d53aa214b1bb14e
MaLkoLmX <kadirovfamily@rambler.ru>
2017-09-17 15:07:32 +0400

    Проверка итогового проекта

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ var close = popup.querySelector(".button-close");
 
 var form = popup.querySelector("form");
 var login = popup.querySelector("[name=name]");
-var password = popup.querySelector("[name=e-mail]");
-var password = popup.querySelector("[name=text]");
+var email = popup.querySelector("[name=e-mail]");
+var email = popup.querySelector("[name=text]");
 
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -17,21 +17,25 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error")
 });
 
 form.addEventListener("submit", function (evt) {
-	if (!name.value || !e-mail.value || !text.value) {
-  evt.preventDefault();
-  popup.classList.add("modal-error");
+	if (!login.value || !email.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
   } else {
-  localStorage.setItem("login", name.value);
+          localStorage.setItem("login", name.value);
   }
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
   if (popup.classList.contains("modal-show")) {
-  popup.classList.remove("modal-show");
-  }
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
   }
 });
